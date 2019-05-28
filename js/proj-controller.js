42779d80967d9a8a5663a701e698f2af1a5e5cdd
Tom Dorofyev <tomdorofey@gmail.com>
2019-05-28 20:13:05 +0300

    changed duplications of modal to rendering by js

--- js/proj-controller.js
+++ js/proj-controller.js
@@ -1,4 +1,16 @@
 $(document).ready(()=>{
     generateProjects();
-
+    $('.portfolio-link').click((ev)=>{
+    renderProjectModal(ev.target.id);
+    })
 })
+
+function renderProjectModal(projectId){
+    var project = getProjById(projectId);
+    $('.portfolio-modal h2').text(project.title);
+    $('.description-modal').text(project.description);
+    $('.modal-image').html(`<img class="img-fluid d-block mx-auto"
+    src="${project.imgUrl}" alt="${project.name} image" />`);
+    $('.modal-project-url').html(`<a href="${project.url}"
+    class="modal-project-url"><button class="btn btn btn-info">Go to project</button></a>`);
+}
